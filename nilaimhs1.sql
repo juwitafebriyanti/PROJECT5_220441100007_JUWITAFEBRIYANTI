6cfcfe0835cc4a257c7d6c7b2bd8740cb60aed44
juwitafebriyanti <113221680+juwitafebriyanti@users.noreply.github.com>
2023-11-09 17:33:16 +0700

    Add files via upload

---
+++ nilaimhs1.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2023 at 11:32 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nilaimhs1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbnilai`
+--
+
+CREATE TABLE `tbnilai` (
+  `nama` varchar(35) NOT NULL,
+  `nim` varchar(35) NOT NULL,
+  `ratarata` varchar(35) NOT NULL,
+  `grade` varchar(35) NOT NULL,
+  `ket` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
